c102f09bb4541a7d9f5020fd221275b6d377e87a
Poupe902 <stablajohnry26@hotmail.com>
2026-01-20 23:47:24 +0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -6,6 +6,12 @@ import CheckoutView from './components/CheckoutView';
 const App: React.FC = () => {
   const [showCheckout, setShowCheckout] = useState(false);
 
+  React.useEffect(() => {
+    if (showCheckout) {
+      window.scrollTo(0, 0);
+    }
+  }, [showCheckout]);
+
   if (showCheckout) {
     return <CheckoutView />;
   }

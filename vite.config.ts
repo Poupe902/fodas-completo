d7bd721a9d44645723dec3d1f23050a39bf933af
Poupe902 <stablajohnry26@hotmail.com>
2026-01-20 09:30:53 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/fadas-completo/',
+  base: './',
+
   build: {
     outDir: 'dist',
     emptyOutDir: true

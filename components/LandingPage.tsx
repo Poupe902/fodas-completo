0b331b2850bd9e9aaf539d5d97450a9a970f3229
Poupe902 <stablajohnry26@hotmail.com>
2026-01-21 14:06:33 +0400

    Update LandingPage.tsx

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -216,7 +216,7 @@ const LandingPage: React.FC<LandingPageProps> = ({ onProceedToCheckout }) => {
             </h2>
             <div className="space-y-6 text-gray-700 leading-relaxed text-lg italic">
               <p>"Imagine colocar uma fada no seu espaço sagrado, como um lembrete diário da sua mente, incentivando você a se reconectar com o seu verdadeiro eu."</p>
-              <img src="" alt="Artesão" className="mx-auto rounded-3xl" />
+              <img src="https://i.ibb.co/gbyGfKRM/Artesao.png" alt="Artesão" className="mx-auto rounded-3xl" />
               <p className="font-bold text-gray-800 not-italic">Com cada fada única, você não está apenas comprando uma bela obra de arte — você também está iniciando uma jornada de autodescoberta e cura.</p>
             </div>
           </div>
@@ -251,7 +251,7 @@ const LandingPage: React.FC<LandingPageProps> = ({ onProceedToCheckout }) => {
                   </div>
                   
                   <div className="w-44 h-32 mb-6 mt-4">
-                     <img src="https://i.ibb.co/gbyGfKRM/Artesao.png" alt="Brinde" className="w-full h-full object-cover rounded-xl filter drop-shadow-md grayscale-[0.2]" />
+                     <img src="https://i.ibb.co/Cs3TFjF3/4ffde935-7254-4725-afee-7efe88dee736.png" alt="Brinde" className="w-full h-full object-cover rounded-xl filter drop-shadow-md grayscale-[0.2]" />
                   </div>
                   
                   <h4 className="font-bold text-gray-800 text-lg mb-1">Amuleto de Cristal Real</h4>

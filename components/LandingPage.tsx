56b6d268cd05cf760a9dab0721e769ec07db346c
Poupe902 <stablajohnry26@hotmail.com>
2026-01-26 17:36:19 -0300

    Update LandingPage.tsx

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useRef } from 'react';
 interface LandingPageProps {
   onProceedToCheckout: () => void;
 }
-<!-- function dispararEventoView() {
+function dispararEventoView() {
   console.log("ENTROU NA FUNCAO")
   if (typeof window.utmifyPixel === "function") {
     console.log("ENTROU NO IF")
@@ -13,9 +13,6 @@ interface LandingPageProps {
     setTimeout(dispararEventoView, 300);
   }
 }
-
-// chama uma vez
-dispararEventoView(); -->
 
 const LandingPage: React.FC<LandingPageProps> = ({ onProceedToCheckout }) => {
   const [currentVariant, setCurrentVariant] = useState('fernheart');
